726b1b6d2fb829d10c6fabfca1db15e7095498f0
R S Madhan <rsmofficial75@gmail.com>
2026-03-30 21:05:05 +0530

    Created first window

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,14 @@ int main()
 	{
 		BeginDrawing();
 
-		ClearBackgound
+		ClearBackground(RAYWHITE);
+
+		DrawText("Congrats! You created your first window!", 190, 200, 20, RED);
+
+		EndDrawing();
 	}
 
+	CloseWindow();
 
 	return 0;
 }
