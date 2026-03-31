fda42796b3e9541d920f7dfe5476639306bbd1de
R S Madhan <rsmofficial75@gmail.com>
2026-03-31 21:14:02 +0530

    added imgui library

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <raylib.h>
+#include  <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
@@ -7,18 +9,34 @@ int main()
 
 	InitWindow(800, 450, "First Window");
 
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
+		rlImGuiBegin();
+
 		//DrawText("Congrats! You created your first window!", 190, 200, 20, RED);
 		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 127});
 		DrawRectangle(75, 75, 100, 100, {0, 255, 0, 127});
 
+		ImGui::ShowDemoWindow();
+		/*ImGui::Begin("test");
+
+		ImGui::Text("hello");
+		ImGui::Button("button");
+
+		ImGui::End();*/
+
+		rlImGuiEnd();
+
 		EndDrawing();
 	}
+
+	rlImGuiShutdown();
 
 	CloseWindow();
 
