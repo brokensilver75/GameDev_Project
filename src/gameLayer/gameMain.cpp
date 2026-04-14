18ac3d36b2632d8c3f553deed1910c959d2c5062
R S Madhan <rsmofficial75@gmail.com>
2026-04-14 10:46:52 +0530

    added asset manager

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,20 +1,28 @@
 #include <raylib.h>
 #include <gameMain.h>
 #include <iostream>
-#include <fstream>
+#include <asserts.h>
+#include <assetManager.h>
 
 struct GameData
 {
 }gameData;
 
+AssetManager asset_manager;
+
 bool initGame()
 {
+	asset_manager.loadAll();
 	return true;
 }
 
 bool updateGame()
 {
-	float deltaTime = GetFrameTime();	
+	float deltaTime = GetFrameTime();
+	if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
+
+	DrawTexturePro(asset_manager.dirt, { 0, 0, (float)asset_manager.dirt.width, (float)asset_manager.dirt.height }, { 50, 50, 100, 100 }, {}, 0, WHITE);
+	
 
 	return true;
 }
