08fd7b63eca9eba9b3d7fb9413c3211737bdfd88
R S Madhan <rsmofficial75@gmail.com>
2026-04-14 06:41:48 +0530

    added assets to resource folder

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -5,9 +5,6 @@
 
 struct GameData
 {
-	float pos_X = 100;
-	float pos_Y = 100;
-	float speed = 100;
 }gameData;
 
 bool initGame()
@@ -17,21 +14,7 @@ bool initGame()
 
 bool updateGame()
 {
-	float deltaTime = GetFrameTime();
-
-	if (deltaTime > 1.f/5)
-	{
-		deltaTime = 1 / 5.f;
-	}
-
-	if (IsKeyDown(KEY_W)) { gameData.pos_Y -= gameData.speed * deltaTime; }
-	if (IsKeyDown(KEY_A)) { gameData.pos_X -= gameData.speed * deltaTime; }
-	if (IsKeyDown(KEY_S)) { gameData.pos_Y += gameData.speed * deltaTime; }
-	if (IsKeyDown(KEY_D)) { gameData.pos_X += gameData.speed * deltaTime; }
-
-
-	DrawRectangle(gameData.pos_X, gameData.pos_Y, 50, 50, RED);
-	//DrawText("Congrats! You created your first window!", 190, 200, 20, RED);
+	float deltaTime = GetFrameTime();	
 
 	return true;
 }
